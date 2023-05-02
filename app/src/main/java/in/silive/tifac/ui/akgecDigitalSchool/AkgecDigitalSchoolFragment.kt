4854ee15b5b47ca2f0c92fcf11fb6e3ea33fb893
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2023-05-02 17:05:34 +0530

    Change font and color of searchView in AkgecDigitalSchoolFragment

--- app/src/main/java/in/silive/tifac/ui/akgecDigitalSchool/AkgecDigitalSchoolFragment.kt
+++ app/src/main/java/in/silive/tifac/ui/akgecDigitalSchool/AkgecDigitalSchoolFragment.kt
@@ -1,16 +1,25 @@
 package `in`.silive.tifac.ui.akgecDigitalSchool
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.viewModels
 import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
+import `in`.silive.tifac.R
 import `in`.silive.tifac.adapters.ViewPagerAdapter
 import `in`.silive.tifac.databinding.FragmentAkgecDigitalSchoolBinding
+import java.lang.reflect.Field
 
 @AndroidEntryPoint
 class AkgecDigitalSchoolFragment : Fragment() {
@@ -22,6 +31,7 @@ class AkgecDigitalSchoolFragment : Fragment() {
 
     private val akgecDigitalSchoolViewModel by viewModels<AkgecDigitalSchoolViewModel>()
 
+    @SuppressLint("DiscouragedPrivateApi")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -50,6 +60,23 @@ class AkgecDigitalSchoolFragment : Fragment() {
             }
 
         })
+
+        val searchText = binding.searchView.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
+        val font = ResourcesCompat.getFont(requireContext(), R.font.gilroy_medium)
+        searchText.typeface = font
+        searchText.setHintTextColor(ContextCompat.getColor(requireContext(), R.color.color9))
+        searchText.setTextColor(ContextCompat.getColor(requireContext(), R.color.color9))
+
+        if(Build.VERSION.SDK_INT >= 29){
+            searchText.textCursorDrawable = AppCompatResources.getDrawable(requireContext(), R.drawable.cursor)
+        }
+        else{
+            try {
+                val f: Field = TextView::class.java.getDeclaredField("mCursorDrawableRes")
+                f.isAccessible = true
+                f.set(searchText, R.drawable.cursor)
+            } catch (_: Exception){}
+        }
 
         return binding.root
     }
