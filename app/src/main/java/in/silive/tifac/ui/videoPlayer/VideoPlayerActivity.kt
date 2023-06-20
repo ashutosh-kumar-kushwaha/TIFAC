69045be12b5561a9d9c9e76f25a644360ec891ad
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2023-06-20 18:53:52 +0530

    Add pip mode

--- app/src/main/java/in/silive/tifac/ui/videoPlayer/VideoPlayerActivity.kt
+++ app/src/main/java/in/silive/tifac/ui/videoPlayer/VideoPlayerActivity.kt
@@ -1,7 +1,11 @@
 package `in`.silive.tifac.ui.videoPlayer
 
+import android.app.PictureInPictureParams
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Rational
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import `in`.silive.tifac.R
 import `in`.silive.tifac.databinding.ActivityVideoBinding
@@ -13,5 +17,20 @@ class VideoPlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_video)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        enterPictureInPictureMode(PictureInPictureParams.Builder()
+                .setAspectRatio(Rational(16,9))
+                .build())
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onBackPressed() {
+        enterPictureInPictureMode(PictureInPictureParams.Builder()
+                .setAspectRatio(Rational(16,9))
+                .build())
     }
 }
---
+++ app/src/main/java/in/silive/tifac/ui/videoPlayer/VideoPlayerViewModel.kt
@@ -0,0 +1,6 @@
+package `in`.silive.tifac.ui.videoPlayer
+
+import androidx.lifecycle.ViewModel
+
+class VideoPlayerViewModel : ViewModel() {
+}
